9c7a443d96e236f2e309eded91d4d375b8600dba
iMatias-ED <matdj31@gmail.com>
2023-02-18 16:06:22 -0300

    Agregado de t-shirts slides

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,13 +23,22 @@ export class AppComponent {
   };
 
   clothesSlidesOptions: SwiperOptions = {
-    spaceBetween: 10,
-    slidesPerView: 2,
+    slidesPerView: 3,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
+    },
+    breakpoints: {
+      "@0.00": { slidesPerView: 1 },
+      "@0.75": { slidesPerView: 2 },
+      "@1.00": { slidesPerView: 3 },
+      "@1.50": { slidesPerView: 4 },
     }
-  };
+  }; 
 
 
 }
